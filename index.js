c2bb27d63b8f3e10c167f0f7ae965551b13d7ca0
emilywhitaker15@gmail.com <emilywhitaker15@gmail.com>
2018-05-22 00:38:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ function currentLine(array) {
     } 
     else if (array.length > 0 && i + 1 === array.length) {
       line = line + ` ${parseInt(i) + 1}. ${array[i]}`
+      return line
     }
     else {
       return "The line is currently empty."
     }
   }
-  return line
 }
